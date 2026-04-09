4c91cd5225740d9aabffd772bfaaa9364b794159
Askirka <vladasker@gmail.com>
2026-04-09 17:42:57 +0300

    first task completed

--- file-organizer/main.go
+++ file-organizer/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var DefaultRules = map[string]string{
+		".jpg":  "Images",
+		".jpeg": "Images",
+		".pdf":  "Documents",
+		".mp3":  "Music",
+	}
+
+	for key, value := range DefaultRules {
+		fmt.Printf("%s: %s\n", strings.ToLower(key), strings.ToLower(value))
+	}
+
+}
